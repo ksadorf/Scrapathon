efd61c2315b148c63749a5e8577a3fa9c54ef3ca
ksad <noe.gaumont@gmail.com>
2013-06-20 19:19:36 +0200

    Changed the readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 Scrapathon
 ==========
+
+This project aims to scrap paris.fr and get some report (pdf or html) and extract some information from it.
+Targeted reports are Municipal or General Council report.
+There are 500 of them per year and with the current site structure, it's impossible to extract informations.
+
+
+
--- scraper.py
+++ scraper.py
@@ -9,21 +9,21 @@ from StringIO import StringIO
 111480
 for id in range(98301, 111480):
 	url = u"http://a06.apps.paris.fr/a06/jsp/site/plugins/solr/modules/ods/DoDownload.jsp?id_document=1"+str(id)
-writer = PdfFileWriter()
-try:
-	remoteFile = urlopen(Request(url)).read()
-	memoryFile = StringIO(remoteFile)
-	pdfFile = PdfFileReader(memoryFile)
-	for pageNum in xrange(pdfFile.getNumPages()):
-	    currentPage = pdfFile.getPage(pageNum)
-	    #currentPage.mergePage(watermark.getPage(0))
-	    writer.addPage(currentPage)
+	writer = PdfFileWriter()
+	try:
+		remoteFile = urlopen(Request(url)).read()
+		memoryFile = StringIO(remoteFile)
+		pdfFile = PdfFileReader(memoryFile)
+		for pageNum in xrange(pdfFile.getNumPages()):
+		    currentPage = pdfFile.getPage(pageNum)
+		    #currentPage.mergePage(watermark.getPage(0))
+		    writer.addPage(currentPage)
 
-	outputStream = open(str(id)+".pdf","wb")
-	writer.write(outputStream)
-	outputStream.close()
-except:
-	print str(id)+" not found"
-	pass
-finally:
-	print str(id)+" Done"
+		outputStream = open(str(id)+".pdf","wb")
+		writer.write(outputStream)
+		outputStream.close()
+	except:
+		print str(id)+" not found"
+		pass
+	finally:
+		print str(id)+" Done"
