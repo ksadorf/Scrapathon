b98c81e7927f64a0a0df53386172f657e5382022
ksad <noe.gaumont@gmail.com>
2013-06-20 18:52:18 +0200

    Add the scraper error have to be handled differently. And support html too...

---
+++ scraper.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python2.7
+# coding: utf-8
+import requests
+from urllib2 import Request, urlopen
+from pyPdf import PdfFileWriter, PdfFileReader
+from StringIO import StringIO
+
+
+111480
+for id in range(98301, 111480):
+	url = u"http://a06.apps.paris.fr/a06/jsp/site/plugins/solr/modules/ods/DoDownload.jsp?id_document=1"+str(id)
+writer = PdfFileWriter()
+try:
+	remoteFile = urlopen(Request(url)).read()
+	memoryFile = StringIO(remoteFile)
+	pdfFile = PdfFileReader(memoryFile)
+	for pageNum in xrange(pdfFile.getNumPages()):
+	    currentPage = pdfFile.getPage(pageNum)
+	    #currentPage.mergePage(watermark.getPage(0))
+	    writer.addPage(currentPage)
+
+	outputStream = open(str(id)+".pdf","wb")
+	writer.write(outputStream)
+	outputStream.close()
+except:
+	print str(id)+" not found"
+	pass
+finally:
+	print str(id)+" Done"
